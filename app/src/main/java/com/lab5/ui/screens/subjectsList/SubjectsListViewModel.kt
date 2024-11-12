9e5abc7d53b5132f159675c47c1c9bd093c8b914
DoNjOnIn <kaijaroslav@gmail.com>
2024-11-12 21:07:30 +0200

    second commit

--- app/src/main/java/com/lab5/ui/screens/subjectsList/SubjectsListViewModel.kt
+++ app/src/main/java/com/lab5/ui/screens/subjectsList/SubjectsListViewModel.kt
@@ -40,40 +40,115 @@ class SubjectsListViewModel(
 
     private suspend fun preloadData() {
         val listOfSubject = listOf(
-            SubjectEntity(id = 1, title = "Subject 1"),
-            SubjectEntity(id = 2, title = "Subject 2"),
-            SubjectEntity(id = 3, title = "Subject 3"),
+            SubjectEntity(id = 1, title = "Мережева безпека"),
+            SubjectEntity(id = 2, title = "Програмування мобільних додатків"),
         )
+        // List of labs
         val listOfSubjectLabs = listOf(
             SubjectLabEntity(
                 id = 1,
                 subjectId = 1,
-                title = "Lab[1] title",
-                description = "Lab[1] description",
-                comment = "Lab[1] comment",
+                title = "Налаштування механізмів безпеки комутаторів ethernet\n",
+                description = "Дослідити принципи налаштування механізмів безпеки на комутаторах Cisco Catalyst",
+                comment = "ноу",
                 isCompleted = true,
             ),
             SubjectLabEntity(
                 id = 2,
                 subjectId = 1,
-                title = "Lab[2] title",
-                description = "Lab[2] description",
+                title = "Оновлення ios, резервне копіювання \n" +
+                        "та відновлення налаштувань комутатора \n" +
+                        "з використанням протоколу tftp\n",
+                description = "Оновлення IOS, резервне копіювання та відновлення налаштувань комутатора з використанням протоколу TFTP",
+                comment = "",
                 inProgress = true,
             ),
             SubjectLabEntity(
                 id = 3,
                 subjectId = 1,
-                title = "Lab[3] title",
-                description = "Lab[3] description",
+                title = "Використання списків контролю доступу acl для управління мережевим трафіком\n",
+                description = "Використання списків контролю доступу ACL для управління мережевим трафіком",
+                comment = "",
+                inProgress = true,
             ),
             SubjectLabEntity(
-                id = 1,
-                subjectId = 2,
-                title = "Lab[1] title",
-                description = "Lab[1] description",
-                comment = "Lab[1] comment"
+                id = 4,
+                subjectId = 1,
+                title = "Відновлення паролів на комутаторах \n" +
+                        "та маршрутизаторах cisco\n",
+                description = "Відновлення паролів на комутаторах та маршрутизаторах Cisco.",
+                comment = "",
+                inProgress = true,
             ),
-        )
+            SubjectLabEntity(
+                id = 5,
+                subjectId = 1,
+                title = "Конфігурація і перевірка IPsec VPN між двома пунктами (site-to-site) за допомогою інтерфейсу командного рядка\n",
+                description = "Конфігурація і перевірка IPsec VPN між двома пунктами (site-to-site) за допомогою інтерфейсу командного рядка.",
+                comment = "",
+                inProgress = true,
+            ),
+            SubjectLabEntity(
+                id = 6,
+                subjectId = 1,
+                title = "Забезпечення безпеки на 2-му рівні\n",
+                description = "Налаштування безпеку на 2 рівні моделі OSI.",
+                comment = "",
+                inProgress = true,
+            ),
+
+            SubjectLabEntity(
+                id = 7,
+                subjectId = 2,
+                title = "Інсталяція середовища Android Studio\n",
+                description = "Інсталювати середовище розробки мобільних додатків Android Studio",
+                comment = "ноу",
+                isCompleted = true,
+            ),
+            SubjectLabEntity(
+                id = 8,
+                subjectId = 2,
+                title = "Вивчення основ Kotlin та Compose UI. Створення простого ToDo\n",
+                description = "Включає розроблений базовий варіант додатку із завдання лабораторної. Можете дивитись і копіювати у свій додаток, але доробіть його зі своїм стилем і фічами, простими словами зробіть красивіше і щось більш схоже на реальний додаток. \n",
+                comment = "ноу",
+                isCompleted = true,
+            ),
+            SubjectLabEntity(
+                id = 9,
+                subjectId = 2,
+                title = "Вивчення навігаційного компонента (Navigation Component) в Android\n",
+                description = "Написати простий додаток, який покаже популярні туристичні напрямки в країні\n" +
+                        "чи місті на ваш вибір. Користувач може переміщатися між різними екранами,\n" +
+                        "щоб переглянути деталі вибраного місця.",
+                comment = "ноу",
+                isCompleted = true,
+            ),
+            SubjectLabEntity(
+                id = 10,
+                subjectId = 2,
+                title = "Робота з базами даних.\n",
+                description = "У цій роботі в завданні запропоновано базу даних \"DataStore\", але у моєму прикладі реалізовано збереження даних через іншу, більш популярну і простішу, базу даних \"Room\".",
+                comment = "ноу",
+                isCompleted = true,
+            ),
+            SubjectLabEntity(
+                id = 11,
+                subjectId = 2,
+                title = "Вивчення архітектури Android додатків та асинхронної роботи\n",
+                description = "Дослідження Koin DI для побудови архітектури дослідити Koin DI інструмент для побудови архітектури в Android",
+                comment = "ноу",
+                isCompleted = true,
+            ),
+            SubjectLabEntity(
+                id = 12,
+                subjectId = 2,
+                title = "Підключення зовнішніх сервісів за допомогою HTTP протоколу\n",
+                description = "Написати простий додаток який буде відображати погоду для певного міста",
+                comment = "ноу",
+                isCompleted = true,
+            ),
+
+            )
 
         listOfSubject.forEach { subject ->
             database.subjectsDao.addSubject(subject)
